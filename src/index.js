4646fdcfb5826922ceaf05acbce521213fdde43e
DimaSurdiy <dsurdiy@ukr.net>
2022-07-18 21:38:14 +0300

    adds functional component

--- src/index.js
+++ src/index.js
@@ -1,31 +1,32 @@
 import ReactDOM from 'react-dom/client';
+import paintings from './paintings.json';
 
-const data = {
-  id: 'id-1',
-  url: 'https://cdn.pixabay.com/photo/2017/07/31/22/05/feathers-2561511_1280.jpg',
-  title: 'Feathers. Art abstract',
-  price: 500,
-  author: {
-    tag: 'ractapopulous',
-    url: 'https://pixabay.com/users/ractapopulous-24766/',
-  },
-  quantity: 10,
-};
+function Painting({ url, title, author, profileUrl, price }) {
+  return (
+    <div>
+      <img src={url} alt={title} width="480" />
+      <h2>{title}</h2>
+      <p>
+        Автор: <a href={profileUrl}>{author}</a>
+      </p>
+      <p>Цена: {price} кредитов</p>
+      <p>Доступность: заканчивается или есть в наличии</p>
+      <button type="button">Добавить в корзину</button>
+    </div>
+  );
+}
 
-const painting = (
-  <div>
-    <img src={data.url} alt={data.title} width="480" />
-    <h2>{data.title}</h2>
-    <p>
-      Автор: <a href={data.author.url}>{data.author.tag}</a>
-    </p>
-    <p>Цена: {data.price} кредитов</p>
-    <p>Доступность: заканчивается или есть в наличии</p>
-    <button type="button">Добавить в корзину</button>
-  </div>
+const painting = paintings[0];
+
+ReactDOM.createRoot(document.querySelector('#root')).render(
+  <Painting
+    url={painting.url}
+    title={painting.title}
+    author={painting.author.tag}
+    profileUrl={painting.author.url}
+    price={painting.price}
+  />,
 );
-
-ReactDOM.createRoot(document.querySelector('#root')).render(painting);
 
 //=================================================================================
 // import React from 'react';
