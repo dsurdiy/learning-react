794ec860f77f086790a8291bdb59ab0c939a6a57
DimaSurdiy <dsurdiy@ukr.net>
2022-07-26 06:02:58 +0300

    ColorPicker component added

---
+++ src/components/ColorPicker/ColorPicker.jsx
@@ -0,0 +1,47 @@
+import { Component } from 'react';
+import './ColorPicker.css';
+
+class ColorPicker extends Component {
+  state = {
+    activeOptionIdx: 3,
+  };
+
+  makeOptionClassName = index => {
+    const optionClasses = ['ColorPicker__option'];
+
+    if (index === this.state.activeOptionIdx) {
+      optionClasses.push('ColorPicker__option--active');
+    }
+
+    return optionClasses.join(' ');
+  };
+
+  setActiveIdx = index => {
+    this.setState({ activeOptionIdx: index });
+  };
+
+  render() {
+    const { options } = this.props;
+    const { activeOptionIdx } = this.state;
+    const { label } = options[activeOptionIdx];
+
+    return (
+      <div className="ColorPicker">
+        <h2 className="ColorPicker__title">Color Picker</h2>
+        <p>Выбран цвет: {label}</p>
+        <div>
+          {options.map(({ label, color }, index) => (
+            <button
+              key={label}
+              className={this.makeOptionClassName(index)}
+              style={{ backgroundColor: color }}
+              onClick={() => this.setActiveIdx(index)}
+            ></button>
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ColorPicker;
