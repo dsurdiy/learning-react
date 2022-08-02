36206d868f092132a3b24cf3b751f69f743e57eb
DimaSurdiy <dsurdiy@ukr.net>
2022-08-02 08:07:42 +0300

    add forms with Formik and Yup libraries

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,118 +1,16 @@
 import { Component } from 'react';
-import { nanoid } from 'nanoid';
-import Container from 'components/Container';
-import TodoList from 'components/TodoList';
-import TodoEditor from 'components/TodoEditor';
-import Filter from 'components/Filter';
-import initialTodos from 'todos.json';
-// import Form from 'components/Form';
+import { GlobalStyle } from './GlobalStyle';
+// import { LoginForm } from './LoginForm/LoginForm';
+import { ProductReviewForm } from './ProductReviewForm/ProductReviewForm';
 
-class App extends Component {
-  state = {
-    todos: initialTodos,
-    filter: '',
-  };
-
-  addTodo = text => {
-    const todo = {
-      id: nanoid(),
-      text,
-      completed: false,
-    };
-
-    this.setState(({ todos }) => ({ todos: [todo, ...todos] }));
-  };
-
-  deleteTodo = todoId => {
-    this.setState(prevState => ({
-      todos: prevState.todos.filter(todo => todo.id !== todoId),
-    }));
-  };
-
-  toggleCompleted = todoId => {
-    // this.setState(prevState => ({
-    //   todos: prevState.todos.map(todo => {
-    //     if (todo.id === todoId) {
-    //       return {
-    //         ...todo,
-    //         completed: !todo.completed,
-    //       };
-    //     }
-
-    //     return todo;
-    //   }),
-    // }));
-
-    this.setState(({ todos }) => ({
-      todos: todos.map(todo =>
-        todo.id === todoId ? { ...todo, completed: !todo.completed } : todo,
-      ),
-    }));
-  };
-
-  changeFilter = e => {
-    this.setState({ filter: e.currentTarget.value });
-  };
-
-  getVisibleTodos = () => {
-    const { todos, filter } = this.state;
-
-    const normalizedFilter = filter.toLowerCase();
-
-    return todos.filter(todo =>
-      todo.text.toLowerCase().includes(normalizedFilter),
-    );
-  };
-
-  calculateCompletedTodos = () => {
-    const { todos } = this.state;
-
-    return todos.reduce(
-      (total, todo) => (todo.completed ? total + 1 : total),
-      0,
-    );
-  };
-
-  // formSubmitHandler = data => {
-  //   console.log(data);
-  // };
-
+export class App extends Component {
   render() {
-    const { todos, filter } = this.state;
-    const totalTodoCount = todos.length;
-    const completedTodoCount = this.calculateCompletedTodos();
-    const visibleTodos = this.getVisibleTodos();
-
     return (
-      <Container>
-        <div>
-          <p>Общее кол-во: {totalTodoCount}</p>
-          <p>Кол-во выполненных: {completedTodoCount}</p>
-        </div>
-
-        <TodoEditor onSubmit={this.addTodo} />
-
-        <Filter value={filter} onChange={this.changeFilter} />
-
-        <TodoList
-          todos={visibleTodos}
-          onDeleteTodo={this.deleteTodo}
-          onToggleCompleted={this.toggleCompleted}
-        />
-
-        {/* <Form onSubmit={this.formSubmitHandler} /> */}
-      </Container>
+      <>
+        <GlobalStyle />
+        {/* <LoginForm /> */}
+        <ProductReviewForm />
+      </>
     );
   }
 }
-
-export default App;
-
-// const colorPickerOptions = [
-//   { label: 'red', color: '#F44336' },
-//   { label: 'green', color: '#4CAF50' },
-//   { label: 'blue', color: '#2196F3' },
-//   { label: 'grey', color: '#607D8B' },
-//   { label: 'pink', color: '#E91E63' },
-//   { label: 'indigo', color: '#3F51B5' },
-// ];
