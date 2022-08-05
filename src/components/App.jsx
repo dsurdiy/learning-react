0f3d56dc9b8f1cde87d78a3a5f0ee88e53165f71
DimaSurdiy <dsurdiy@ukr.net>
2022-08-05 17:52:22 +0300

    update App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,94 @@
 import { Component } from 'react';
-import { ToastContainer } from 'react-toastify';
-import { PokemonForm } from './PokemonForm';
-import { PokemonInfo } from './PokemonInfo';
+import { GlobalStyle } from './GlobalStyle';
+import { Layout } from './Layout';
+import { MaterialEditorForm } from './MaterialEditorForm/MaterialEditorForm';
+import { MaterialList } from './MaterialList/MaterialList';
+import * as API from 'services/api';
 
 export class App extends Component {
   state = {
-    pokemonName: '',
+    materials: [],
+    isLoading: false,
+    error: false,
   };
 
-  handleFormSubmit = pokemonName => {
-    this.setState({ pokemonName });
+  async componentDidMount() {
+    try {
+      this.setState({ isLoading: true });
+
+      const materials = await API.getMaterials();
+
+      this.setState({ materials, isLoading: false });
+    } catch (error) {
+      this.setState({ error: true, isLoading: false });
+      console.log(error);
+    }
+  }
+
+  addMaterial = async values => {
+    try {
+      const material = await API.addMaterial(values);
+
+      this.setState(state => ({
+        materials: [...state.materials, material],
+      }));
+    } catch (error) {
+      this.setState({ error: true, isLoading: false });
+      console.log(error);
+    }
+  };
+
+  deleteMaterial = async id => {
+    try {
+      await API.deleteMaterial(id);
+
+      this.setState(state => ({
+        materials: state.materials.filter(material => material.id !== id),
+      }));
+    } catch (error) {
+      this.setState({ error: true });
+      console.log(error);
+    }
+  };
+
+  updateMaterial = async fields => {
+    try {
+      const updatedMaterial = await API.updateMaterial(fields);
+
+      this.setState(state => ({
+        materials: state.materials.map(material =>
+          material.id === fields.id ? updatedMaterial : material,
+        ),
+      }));
+    } catch (error) {
+      this.setState({ error: true });
+      console.log(error);
+    }
   };
 
   render() {
-    return (
-      <div>
-        <PokemonForm onSubmit={this.handleFormSubmit} />
-        <PokemonInfo pokemonName={this.state.pokemonName} />
+    const { materials, isLoading, error } = this.state;
 
-        <ToastContainer autoClose={3000} />
-      </div>
+    return (
+      <Layout>
+        <GlobalStyle />
+
+        {error && (
+          <p>Упс... щось пішло не так :( Спробуйте перезавантажити сторінку</p>
+        )}
+
+        <MaterialEditorForm onSubmit={this.addMaterial} />
+
+        {isLoading ? (
+          'Завантажуємо матеріали...'
+        ) : (
+          <MaterialList
+            items={materials}
+            onDelete={this.deleteMaterial}
+            onUpdate={this.updateMaterial}
+          />
+        )}
+      </Layout>
     );
   }
 }
