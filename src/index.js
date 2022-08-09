fdc3125d449808e0f4597e058ff977532d3fe234
DimaSurdiy <dsurdiy@ukr.net>
2022-08-09 21:30:08 +0300

    lesson-07 starting files added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from './components/App';
+import App from './App';
+import 'modern-normalize/modern-normalize.css';
+import './index.css';
+import 'react-toastify/dist/ReactToastify.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
