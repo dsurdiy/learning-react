7af15fa47fb83f9a5233de9391676d4e48989b60
DimaSurdiy <dsurdiy@ukr.net>
2022-08-18 08:53:20 +0300

    add UserMenu

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,28 @@
-// import { Switch, Route } from 'react-router-dom';
-// import AppBar from './components/AppBar/AppBar';
-// import SignupForm from './components/SignupForm/SignupForm';
-// import ColorPicker from './components/ColorPicker/ColorPicker';
-// import Counter from './components/Counter/Counter';
-// import Clock from './components/Clock/Clock';
-// import PokemonView from './views/PokemonView';
-// import SkipEffectOnFirstRender from './components/SkipEffectOnFirstRender';
-import Friends from './Friends';
+// import { Clock } from 'components/Clock';
+// import { Counter } from 'components/Counter';
+// import { SingUpForm } from 'components/SingUpForm';
+// import ColorPicker from 'components/ColorPicker/ColorPicker';
+// import { News } from 'components/News';
+import { UserMenu } from 'components/UserMenu';
 
-// const colorPickerOptions = [
-//   { label: 'red', color: '#F44336' },
-//   { label: 'green', color: '#4CAF50' },
-//   { label: 'blue', color: '#2196F3' },
-//   { label: 'grey', color: '#607D8B' },
-//   { label: 'pink', color: '#E91E63' },
-//   { label: 'indigo', color: '#3F51B5' },
-// ];
+const colorPickerOptions = [
+  { label: 'red', color: '#F44336' },
+  { label: 'green', color: '#4CAF50' },
+  { label: 'blue', color: '#2196F3' },
+  { label: 'grey', color: '#607D8B' },
+  { label: 'pink', color: '#E91E63' },
+  { label: 'indigo', color: '#3F51B5' },
+];
 
-const containerStyles = {
-  maxWidth: 1170,
-  marginLeft: 'auto',
-  marginRight: 'auto',
-  paddingLeft: 15,
-  paddingRight: 15,
-};
-
-export default function App() {
+export const App = () => {
   return (
-    <div style={containerStyles}>
-      {/* <Counter /> */}
-      {/* <SignupForm /> */}
+    <>
       {/* <Clock /> */}
-      {/* <SkipEffectOnFirstRender /> */}
-      {/* <PokemonView /> */}
-      <Friends />
-    </div>
+      {/* <Counter /> */}
+      {/* <SingUpForm /> */}
+      {/* <ColorPicker options={colorPickerOptions} /> */}
+      {/* <News /> */}
+      <UserMenu />
+    </>
   );
-}
+};
