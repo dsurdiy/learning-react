31b4208186c265de31312ccb9e13907cf0492416
DimaSurdiy <dsurdiy@ukr.net>
2022-08-20 17:50:08 +0300

    custom hook useModal added

---
+++ src/hooks/useModal.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+
+const useModal = (initialState = false) => {
+  const [isModalOpen, setIsModalOpen] = useState(initialState);
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
+
+  return { isModalOpen, openModal, closeModal };
+};
+
+export const MyCompA = () => {
+  const { isModalOpen, openModal, closeModal } = useModal(true);
+
+  return null;
+};
+
+export const MyCompB = () => {
+  const { isModalOpen, openModal, closeModal } = useModal();
+
+  return null;
+};
+
+export const MyCompC = () => {
+  const { isModalOpen, openModal, closeModal } = useModal();
+
+  return null;
+};
