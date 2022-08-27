82e2492d2198da672a5a1991bda3a7b0778afbf1
DimaSurdiy <dsurdiy@ukr.net>
2022-08-27 22:41:52 +0300

    update App with code splitting

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,28 @@
+import { lazy, Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { Layout } from './components/Layout/Layout';
-import { HomeView } from './pages/HomeView';
-import { AuthorsView } from './pages/AuthorsView';
-import { AuthorSubView } from './pages/AuthorSubView';
-import { BooksView } from './pages/BooksView';
-import { BookDetailsView } from './pages/BookDetailsView';
-import { NotFoundView } from './pages/NotFoundView';
+
+const HomeView = lazy(() => import('./pages/HomeView'));
+const BooksView = lazy(() => import('./pages/BooksView'));
+const AuthorsView = lazy(() => import('./pages/AuthorsView'));
+const AuthorSubView = lazy(() => import('./pages/AuthorSubView'));
+const BookDetailsView = lazy(() => import('./pages/BookDetailsView'));
+const NotFoundView = lazy(() => import('./pages/NotFoundView'));
 
 export const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<HomeView />} />
-        <Route path="authors" element={<AuthorsView />}>
-          <Route path=":authorId" element={<AuthorSubView />} />
+    <Suspense fallback={<h1>Loading...</h1>}>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomeView />} />
+          <Route path="authors" element={<AuthorsView />}>
+            <Route path=":authorId" element={<AuthorSubView />} />
+          </Route>
+          <Route path="books" element={<BooksView />} />
+          <Route path="books/:bookId" element={<BookDetailsView />} />
+          <Route path="*" element={<NotFoundView />} />
         </Route>
-        <Route path="books" element={<BooksView />} />
-        <Route path="books/:bookId" element={<BookDetailsView />} />
-        <Route path="*" element={<NotFoundView />} />
-      </Route>
-    </Routes>
+      </Routes>
+    </Suspense>
   );
 };
