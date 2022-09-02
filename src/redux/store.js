ebc016bfc3c24793421373dfdeab2abab223fc46
DimaSurdiy <dsurdiy@ukr.net>
2022-09-02 12:59:20 +0300

    redux store added

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,26 +1,25 @@
-import { configureStore, createReducer, createAction } from "@reduxjs/toolkit";
-
-export const increment = createAction("myValue/increment");
-export const decrement = createAction("myValue/decrement");
-
-const myReducer = createReducer(0, {
-  [increment]: (state, action) => state + action.payload,
-  [decrement]: (state, action) => state - action.payload,
-});
-
-const add = createAction("items/add");
-const remove = createAction("items/remove");
-
-const itemsReducer = createReducer([], {
-  //   [add]: (state, action) => [...state, action.payload],
-  [add]: (state, action) => state.push(action.payload),
-  [remove]: (state, action) =>
-    state.filter((item) => item.id !== action.payload),
-});
+import { configureStore } from '@reduxjs/toolkit';
+import { userSlice } from './userSlice';
 
 export const store = configureStore({
   reducer: {
-    myValue: myReducer,
-    items: itemsReducer,
+    user: userSlice.reducer,
   },
 });
+
+// export const increment = createAction('myValue/increment');
+// export const decrement = createAction('myValue/decrement');
+
+// const myReducer = createReducer(0, {
+//   [increment]: (state, action) => state + action.payload,
+//   [decrement]: (state, action) => state - action.payload,
+// });
+
+// const add = createAction('items/add');
+// const remove = createAction('items/remove');
+
+// const itemsReducer = createReducer([], {
+//   add]: (state, action) => [...state, action.payload],
+//   [add]: (state, action) => state.push(action.payload),
+//   [remove]: (state, action) => state.filter(item => item.id !== action.payload),
+// });
