610a6ddb411171b195def983cad7fc5dad5068d8
DimaSurdiy <dsurdiy@ukr.net>
2022-09-04 23:29:54 +0300

    add Timer

--- src/index.js
+++ src/index.js
@@ -1,21 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { ThemeProvider } from 'styled-components';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { App } from './components/App';
-import { store } from './redux/store';
-import { theme } from './constants/theme';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { Timer } from "./Timer";
+import { store } from "./redux/store";
+import "./global.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <Provider store={store}>
-          <App />
-        </Provider>
-      </ThemeProvider>
-    </BrowserRouter>
-  </React.StrictMode>,
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <Timer />
+  </Provider>
 );
