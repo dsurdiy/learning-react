bf1758c8f38d229719e73032771abc0a72b904ea
DimaSurdiy <dsurdiy@ukr.net>
2022-09-08 23:44:41 +0300

    add store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
-import booksReducer from './books/booksReducer';
+import { setupListeners } from '@reduxjs/toolkit/query';
+import { pokemonApi } from './pokemon';
 
 export const store = configureStore({
   reducer: {
-    books: booksReducer,
+    [pokemonApi.reducerPath]: pokemonApi.reducer,
   },
+  middleware: getDefaultMiddleware => [
+    ...getDefaultMiddleware(),
+    pokemonApi.middleware,
+  ],
 });
+
+setupListeners(store.dispatch);
