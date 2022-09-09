fe88208e9167dabd8d512da0754ae66a2b02a238
DimaSurdiy <dsurdiy@ukr.net>
2022-09-09 23:25:27 +0300

    update store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/query';
 import { pokemonApi } from './pokemon';
+import { todoApi } from './todos/todoSlice';
 
 export const store = configureStore({
   reducer: {
     [pokemonApi.reducerPath]: pokemonApi.reducer,
+    [todoApi.reducerPath]: todoApi.reducer,
   },
   middleware: getDefaultMiddleware => [
     ...getDefaultMiddleware(),
     pokemonApi.middleware,
+    todoApi.middleware,
   ],
 });
 
