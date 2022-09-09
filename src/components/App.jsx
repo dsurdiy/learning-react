fb49cab4473b4c4ea054457f67970e6d6fe6996a
DimaSurdiy <dsurdiy@ukr.net>
2022-09-09 23:26:02 +0300

    update App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,18 @@
+import { Toaster } from 'react-hot-toast';
 import { Routes, Route } from 'react-router-dom';
 import { HomePage } from 'pages/Home';
+import { TodosPage } from 'pages/Todos';
+import { CreateTodoPage } from 'pages/CreateTodo';
 
 export const App = () => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/todos" element={<TodosPage />} />
+        <Route path="/todos/create" element={<CreateTodoPage />} />
       </Routes>
+      <Toaster position="top-right" />
     </div>
   );
 };
