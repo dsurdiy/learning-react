2be5d46a976cdb422e7f98ddd973c44d260f60ee
DimaSurdiy <dsurdiy@ukr.net>
2022-09-13 20:59:21 +0300

    add redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,39 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { materialsApi } from './materialsSlice';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { todosReducer } from './todos';
+import { authReducer } from './auth';
+
+const middleware = [
+  ...getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+];
+
+const authPersistConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['token'],
+};
 
 export const store = configureStore({
   reducer: {
-    [materialsApi.reducerPath]: materialsApi.reducer,
+    auth: persistReducer(authPersistConfig, authReducer),
+    todos: todosReducer,
   },
-  middleware: getDefaultMiddleware => [
-    ...getDefaultMiddleware(),
-    materialsApi.middleware,
-  ],
+  middleware,
+  devTools: process.env.NODE_ENV === 'development',
 });
+
+export const persistor = persistStore(store);
