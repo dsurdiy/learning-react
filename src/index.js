e9172c1ee77855541b5bcd28e6180c1dd39c3a15
DimaSurdiy <dsurdiy@ukr.net>
2022-09-13 20:59:37 +0300

    update App.jsx, index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { App } from 'components/App';
-import { store } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import App from './App';
+import { store, persistor } from './redux/store';
+import 'modern-normalize/modern-normalize.css';
+import './styles/base.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename="/react-41/">
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
-  </React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
