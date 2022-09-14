9c9758092bc8fa0e04ee283fe76b2e384a618dae
DimaSurdiy <dsurdiy@ukr.net>
2022-09-14 12:34:17 +0300

    App updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-// import { useEffect } from 'react';
-// import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { Switch, Route } from 'react-router-dom';
 import AppBar from './components/AppBar';
 import TodosView from './views/TodosView';
@@ -7,9 +7,15 @@ import HomeView from './views/HomeView';
 import RegisterView from './views/RegisterView';
 import LoginView from './views/LoginView';
 import Container from './components/Container';
-// import { authOperations } from './redux/auth';
+import { authOperations } from './redux/auth';
 
 export default function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(authOperations.fetchCurrentUser());
+  }, [dispatch]);
+
   return (
     <Container>
       <AppBar />
